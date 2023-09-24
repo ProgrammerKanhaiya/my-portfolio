7ab5e65e7c5b7724c4e9c1eb7872de902eadc2e9
Kanhaiya <kanhaiya.sahani@niveussolutions.com>
2023-09-24 08:19:40 +0530

    new brach added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import DarkMode from './DarkMode';
-import {ColorContext} from '../Context/ColorContex'
+import { ColorContext } from '../Context/ColorContex'
 export default function Header() {
 
-    const {color , setColor} = React.useContext(ColorContext)
-    console.log('color',color);
+    const { color, setColor } = React.useContext(ColorContext)
+
     return (
         <header className="shadow sticky z-50 top-0 dark-cl">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
@@ -26,7 +26,7 @@ export default function Header() {
                         </Link>
                         <Link
                             to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" 
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Get started
                         </Link>
@@ -75,7 +75,7 @@ export default function Header() {
                                 >
                                     Github
                                 </NavLink>
-        
+
                             </li>
                             {/* <li className="cursor-pointer">
                                 DarkMode
