44b05de4f27c2984bedeec3cd588aef92fc64dce
Kanhaiya <kanhaiya.sahani@niveussolutions.com>
2023-09-27 16:41:35 +0530

    card changes

--- src/Components/Projects/Cards.jsx
+++ src/Components/Projects/Cards.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 
 const Cards = (props) => {
     let tech;
-    if(props.technology !== undefined){
-      tech = JSON.parse(props.technology)
+    if (props.technology !== undefined) {
+        tech = JSON.parse(props.technology)
 
     }
     console.log('technology =', tech);
     return (
-        <div className='bg-gray-100 h-[400px] shadow-md m-4 hover:cursor-pointer hover:scale-105 transition-all ease-in-out '>
-            <h1 className='text-xl font-bold text-[#3734e6] text-center'>{props.name}</h1>
+        <div className='bg-gray-100 min-h-[400px] shadow-md m-4 hover:cursor-pointer hover:scale-105 transition-all ease-in-out px-4'>
+            <div className="w-full h-10">
+                {props.image && <img src={props.image} alt="Project Image" className='w-25 h-10 m-auto rounded-lg' />}
+
+            </div>
+            <h1 className='text-xl font-bold text-[#3734e6] pt-4'><span>Project Name : &nbsp; </span>{props.name}</h1>
             {props.role && <span>Worked as a :&nbsp;&nbsp;{props.role}</span>}
-            { tech && <span>Technology Used :</span>} <ul>
+            {props.description && <p className='text-gray-500 pt-4 text-justify'>{props.description}</p>}
+            {tech && <div className='pt-3'>Technology Used &nbsp; :</div>}
+            {tech && <ul className='pt-2'>
                 {
-                    tech && tech.map((items , index)=>{
-                        return(
+                    tech && tech.map((items, index) => {
+                        return (
                             <li className='text-gray-500' key={index}>{items}</li>
                         )
                     })
                 }
             </ul>
+            }
         </div>
     )
 
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,8 +6,15 @@ import Cards from './Cards'
 const projects = [
   {
     "id" : 1,
-    "name": "ICICI - INSTA SAVE ACCOUNT",
-    "role" : "Full Stack Developer"
+    "name": "ICICI - Projects",
+    "role" : "Full Stack Developer",
+    "image" : "https://upload.wikimedia.org/wikipedia/commons/1/12/ICICI_Bank_Logo.svg",
+    "technology" :[
+      "Node js ",
+      "React js",
+      "MySQl",
+    ],
+    "description" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived"
   },
   {
     "id" : 2,
@@ -34,12 +41,12 @@ const projects = [
 
 const Projects = ()=> {
   return (
-    <div className="p-5 grid grid-cols-1 md:grid-cols-3 shadow-transparent ">
+    <div className="pt-4 grid grid-cols-1 md:grid-cols-3 shadow-transparent px-10 ">
       {
         projects.map((items)=>{
           console.log('items =',items);
           return(
-            <Cards key={items.id} name={items.name} technology={JSON.stringify(items.technology)} role={items.role?items.role:null }/>
+            <Cards key={items.id} name={items.name} image={items.image} technology={JSON.stringify(items.technology)} role={items.role?items.role:null } description ={items.description}/>
           )
         })
       }
